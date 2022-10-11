e3c3b71da2d12393058b9294827face5b9164615
ratulislam <iratul010@gmail.com>
2022-10-12 03:32:18 +0600

    statistics-add

--- src/Component/Statistics/Statistics.js
+++ src/Component/Statistics/Statistics.js
@@ -7,8 +7,26 @@ const Statistics = () => {
   console.log(userData);
 
   return (
-    <div className="container mx-auto bg-green-50 mt-12">
-      <h2>statistics</h2>
+    <div className="container mx-auto bg-green-50 mt-12 md:flex-col items-center justify-center">
+      <div className="m-12 md:text-3xl">
+        <h2>React,Javascript,CSS & Git Total Question Recharts</h2>
+      </div>
+
+      <div>
+        <LineChart
+          width={300}
+          className="md:w-[600px]"
+          height={300}
+          data={userData}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </div>
     </div>
   );
 };
