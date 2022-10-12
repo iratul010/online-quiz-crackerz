fc48e85a6a406263ba4853464a4f464e7756b870
ratulislam <iratul010@gmail.com>
2022-10-12 16:29:33 +0600

    full-done

--- src/Component/McqQuestions/McqQuestions.js
+++ src/Component/McqQuestions/McqQuestions.js
@@ -38,7 +38,16 @@ const McqQuestions = (mcq) => {
             <Mcq key={index} option={option} answer={answer} correctAnswer={correctAnswer}></Mcq>
           ))}
         </div>
-        <div className={`${set ? "hidden" : "visible"}`}>
+        <div className={`${set ? "hidden" : "visible"} flex justify-center items-center`}>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-green-400">
+              <path
+                fillRule="evenodd"
+                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
           <p>{correctAnswer}</p>
         </div>
       </div>
